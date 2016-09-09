2833e1e4569323ff0c5453405b538cc6904fe087
leton8888 <zhoumingyang@gmail.com>
2016-09-09 18:04:58 +0800

    文件导入
    文件导入

---
+++ Assets/Editor/CreateWallEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(CreateWallScript))]
+public class CreateWallEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        CreateWallScript cwScript = (CreateWallScript)target;
+        if (GUILayout.Button("Create Wall")) {
+            cwScript.CreateWall();
+        }
+    }
+}
---
+++ Assets/Scripts/CreateWallScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateWallScript : MonoBehaviour
+{
+    public GameObject wallObj;
+    public GameObject startPoint;
+
+    public void CreateWall()
+    {
+        //float _x = startPoint.transform.position.x;
+        //float _y = startPoint.transform.position.y;
+
+        for (int j = 0; j < 50; j++)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Instantiate(wallObj, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
+
+
+    }
+}
